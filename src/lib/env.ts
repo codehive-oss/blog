f1881639a959d14389703a34bafc8c2b57d39445
Eugene Matsumura <eugene.bb1@gmail.com>
2023-01-09 22:18:59 +0100

    style: format code

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,1 +1,6 @@
-export const WP_URL = process.env.WORDPRESS_URL!.endsWith("/") ? process.env.WORDPRESS_URL?.substring(0, process.env.WORDPRESS_URL.length - 1) : process.env.WORDPRESS_URL;
+export const WP_URL = process.env.WORDPRESS_URL!.endsWith("/")
+  ? process.env.WORDPRESS_URL?.substring(
+      0,
+      process.env.WORDPRESS_URL.length - 1,
+    )
+  : process.env.WORDPRESS_URL;
