ed3c95f0d75c5fba4555fcf6e5636fe41045f01d
Eugene Matsumura <eugene.bb1@gmail.com>
2023-01-10 14:28:30 +0100

    feat: add configurable global css file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,12 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+  ],
   output: "server",
   adapter: node({
     mode: "standalone",
