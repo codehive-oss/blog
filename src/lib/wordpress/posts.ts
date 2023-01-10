1d7c435829f4f10082460c22fb67caeb65155de5
Amin <amin.haddou@frg.wwschool.de>
2023-01-10 12:19:45 +0100

    dont crash if post doesnt exist

--- src/lib/wordpress/posts.ts
+++ src/lib/wordpress/posts.ts
@@ -2,9 +2,13 @@ import axios from "axios";
 import type { WP_REST_API_Post, WP_REST_API_Posts } from "wp-types";
 import { routes } from "./routes";
 
-export const getPost = async (id: number): Promise<WP_REST_API_Post> => {
-  const resp = await axios.get(routes.post(id));
-  return resp.data;
+export const getPost = async (id: number): Promise<WP_REST_API_Post | null> => {
+  try {
+    const resp = await axios.get(routes.post(id));
+    return resp.data;
+  } catch (e) {
+    return null;
+  }
 };
 
 export const getAllPosts = async (): Promise<WP_REST_API_Posts> => {
