82d1c49b486bcccc1d8334f691f6e91c6405f063
Amin <amin.haddou@frg.wwschool.de>
2023-01-11 21:32:04 +0100

    fix 404

--- server/server.js
+++ server/server.js
@@ -2,17 +2,27 @@
 import express from "express";
 import { exec } from "child_process";
 import cron from "node-cron";
-import * as fs from "fs-extra";
+import * as fse from "fs-extra";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-fs.copy("dist", "target", { overwrite: true });
+fse.copy("dist", "target", { overwrite: true });
+
 app.use(express.static("./target"));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "/../dist/404.html"));
+});
 
 cron.schedule("*/10 * * * *", () => {
   console.log("rebuilding");
   exec("yarn build");
-  fs.copy("dist", "target", { overwrite: true });
+  fse.copy("dist", "target", { overwrite: true });
 });
 
 const port = process.env.SERVER_PORT ?? 3000;
